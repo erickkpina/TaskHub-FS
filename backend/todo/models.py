d25750842d9b887d819b53cf472db888c4103819
erick <a22006182@alunos.ulht.pt>
2023-09-25 20:22:55 +0100

    change model start_date and end_date to add time

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -5,8 +5,8 @@ class Todo(models.Model):
    title = models.CharField(max_length=100)
    description = models.TextField()
    completed = models.BooleanField(default=False)
-   start_date = models.CharField(max_length=10, default=timezone.now().strftime('%d/%m/%Y'))
-   end_date = models.CharField(max_length=10, default=timezone.now().strftime('%d/%m/%Y'))
+   start_date = models.CharField(max_length=50, default=timezone.now().strftime('%d/%m/%Y - %H:%M'))
+   end_date = models.CharField(max_length=50, default=timezone.now().strftime('%d/%m/%Y - %H:%M'))
 
    def __str__(self):
      return self.title
